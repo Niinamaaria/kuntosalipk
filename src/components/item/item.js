036e95daf08566fd2279b07d835096818fcd1348
Niinamaaria <niina.kettunen@edu.sasky.fi>
2021-02-06 14:04:03 +0200

    Testdata added

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -2,13 +2,17 @@ import styles from './item.module.scss';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 function Item(props) {
+
+    const locale ="fi-FI";
+    const trainDate = new Date(props.data.trainDate).toLocaleDateString(locale);
+
     return (
         <div className={styles.item}>
             <div className={styles.item_data}>
-                <div className={styles.item_type}>Syväkyykky</div>
-                <div className={styles.item_repeats}>3 x 15</div>
-                <div className={styles.item_date}>30.1.2021</div>
-                <div className={styles.item_kg}>50 kg</div>
+                <div className={styles.item_type}>{props.data.type}</div>
+                <div className={styles.item_repeats}>{props.data.repeats}</div>
+                <div className={styles.item_date}>{trainDate}</div>
+                <div className={styles.item_kg}>{props.data.kilos}</div>
             </div>
             <div className={styles.item_edit}>
                 <NavigateNextIcon />
---
+++ src/testdata.js
@@ -0,0 +1,39 @@
+const data = [
+    {
+        id:           "1",
+        type:         "Syväkyykky",
+        repeats:      "3 x 15",
+        trainDate:    "2021-2-6",
+        kilos:        "50 kg"
+    },
+    {
+        id:           "2",
+        type:         "Hauiskääntö",
+        repeats:      "4 x 8",
+        trainDate:    "2021-1-6",
+        kilos:        "10 kg"
+    },
+    {
+        id:           "3",
+        type:         "Maastaveto",
+        repeats:      "5 x 8",
+        trainDate:    "2020-12-6",
+        kilos:        "30 kg"
+    },
+    {
+        id:           "4",
+        type:         "Alatalja",
+        repeats:      "3 x 15",
+        trainDate:    "2020-11-6",
+        kilos:        "30 kg"
+    },
+    {
+        id:           "5",
+        type:         "Lantionnosto",
+        repeats:      "4 x 10",
+        trainDate:    "2020-10-6",
+        kilos:        "25 kg"
+    }
+];
+
+export default data;
